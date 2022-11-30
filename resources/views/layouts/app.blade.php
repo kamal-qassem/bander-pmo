ef0f9bd88c8fba8188dea565111ace7255b1be15
kamal-khalil@live.com <kamal-khalil@live.com>
2022-11-30 23:37:29 +0200

    11:37

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,12 @@ if (\Cookie::get('color_skin')) { // If user has his own skin we need get it.
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
     @include('layouts.simple.css')
-     {{ $siteTitle }}
+  @if(isset($siteTitle))
+                <h3 class="page-title">
+                    {{ $siteTitle }}
+                </h3>
+            @endif
+
     @yield('style')
   </head>
   <body @if(Route::current()->getName() == 'index') onload="startTime()" @endif>
