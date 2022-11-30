19ed19570e66df11e1c61e9d2e6ba51a878cbb4c
kamal-khalil@live.com <kamal-khalil@live.com>
2022-11-30 19:42:39 +0200

    'afterrec123688'

---
+++ public/assets/js/main.js
@@ -0,0 +1,106 @@
+//hide and show form in the same page
+
+/* On click toggle signin & Create Account Button */
+$('.toggleBtn').click(function () {
+$('.toggleBtn').toggle();
+})
+
+/* On click SignIn button */
+$('.loginBtn').click(function () {
+$('#signUp').hide();
+$('#signIn').show();
+})
+
+
+/* On click create account button */
+$('.signUpBtn').click(function () {
+$('#signUp').show();
+$('#signIn').hide();
+$('.loginBtn').show(); 
+})
+
+/* On click hide/show forms on mobile */
+$('.loginBtn-mobile').click(function () {
+$('#signUp').hide();
+$('#signIn').show();
+})
+
+$('.signUpBtn-mobile').click(function () {
+$('#signUp').show();
+$('#signIn').hide();
+})
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+function validateForm() {  
+    //collect form data in JavaScript variables  
+    var pw1 = document.getElementById("pswd1").value;  
+    var pw2 = document.getElementById("pswd2").value;  
+ 
+    
+    //check empty password field  
+    if(pw1 == "") {  
+      document.getElementById("message1").innerHTML = "**Fill the password please!";  
+      return false;  
+    }  
+    
+    //check empty confirm password field  
+    if(pw2 == "") {  
+      document.getElementById("message2").innerHTML = "**Enter the password please!";  
+      return false;  
+    }   
+     
+    //minimum password length validation  
+    if(pw1.length < 8) {  
+      document.getElementById("message1").innerHTML = "**Password length must be atleast 8 characters";  
+      return false;  
+    }  
+  
+    //maximum length of password validation  
+    if(pw1.length > 15) {  
+      document.getElementById("message1").innerHTML = "**Password length must not exceed 15 characters";  
+      return false;  
+    }  
+    
+    if(pw1 != pw2) {  
+      document.getElementById("message2").innerHTML = "**Passwords are not same";  
+      return false;  
+    } else {  
+      alert ("Your password created successfully");  
+      document.write("JavaScript form has been submitted successfully");  
+    }  
+  
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//animations
+setTimeout(function(){
+  $('.login-block').addClass('magictime puffIn');
+}, 5);
+setTimeout(function(){
+  $('.loginForm').addClass('magictime spaceInRight');
+});
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///backgroiund refresh"
+function randomImage(){
+  var images = [
+    "assets/img/background.jpeg", 
+    "assets/img/1.jpg",
+    "assets/img/2.jpg",
+    "assets/img/3.jpg",
+    "assets/img/4.jpg",
+    "assets/img/5.jpg",
+    "assets/img/6.jpg",
+    "assets/img/7.jpg",
+    "assets/img/8.jpg",
+    "assets/img/9.jpg",
+    "assets/img/10.jpg",
+ ];
+  var size = images.length;
+  var x = Math.floor(size * Math.random());
+  var element = document.getElementsByClassName('login-block');
+  element[0].style["background-image"] = "url("+ images[x] + ")";
+}
+
+document.addEventListener("DOMContentLoaded", randomImage);
